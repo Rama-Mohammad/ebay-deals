473f4d2b8ab13a4331c56ab11586e3e661a22e62
Rama <rama_marwan@hotmail.com>
2026-03-14 00:16:03 +0200

    uncomment headless

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ import time
 # setup for driver
 def setup_driver():
     options = Options()
-    #options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
